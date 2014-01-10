418dc38d72656a36505e4f5e57e50c615a1c3bbc
Stas SUȘCOV <stas@net.utcluj.ro>
2014-01-10 14:09:10 +0200

    Use the spec helper.

--- spec/socialable_spec.rb
+++ spec/socialable_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path("../../lib/redcarpet/socialable", __FILE__)
+require 'spec_helper'
 
 class NoTags < Redcarpet::Socialable
   def highlight_tag?(name)
@@ -29,71 +29,71 @@ describe Redcarpet::Socialable do
   let(:markdown) do
     Redcarpet::Markdown.new(renderer, space_after_headers: true)
   end
-  
+
   it "returns html" do
     sample = "foo\n\nbar"
     expect(markdown.render(sample)).to match(/</)
   end
-  
+
   { "@mentions" => "@", "#hashtags" => "#" }.each do |type, char|
     context "highlighting #{type}" do
       subject { markdown.render(sample) }
-    
+
       context "in paragraphs" do
         let(:sample) { "#{char}bar" }
-        
+
         it { expect(subject).to match(/<a/) }
       end
-    
+
       context "in lists" do
         let(:sample) { "* foo\n* #{char}bar\n* buz" }
-        
+
         it { expect(subject).to match(/<a/) }
       end
-      
+
       context "in blockquote" do
         let(:sample) { "> foo #{char}bar buz" }
-        
+
         it { expect(subject).to match(/<a/) }
       end
-      
+
       context "in headers" do
         let(:sample) { "# foo #{char}bar buz" }
-        
+
         it { expect(subject).to match(/<a/) }
       end
     end
   end
-  
+
   describe "override #highlight_tag?" do
     let(:renderer) { NoTags.new }
     let(:sample) { "#foo" }
     subject { markdown.render(sample) }
-    
+
     it { expect(subject).not_to match(/<a/) }
   end
-  
+
   describe "override #highlight_mention?" do
     let(:renderer) { NoMentions.new }
     let(:sample) { "@foo" }
     subject { markdown.render(sample) }
-    
+
     it { expect(subject).not_to match(/<a/) }
   end
-  
+
   describe "override #tag_template?" do
     let(:renderer) { SpanTags.new }
     let(:sample) { "#foo" }
     subject { markdown.render(sample) }
-    
+
     it { expect(subject).to match(/<span/) }
   end
-  
+
   describe "override #mention_template" do
     let(:renderer) { SpanMentions.new }
     let(:sample) { "@foo" }
     subject { markdown.render(sample) }
-    
+
     it { expect(subject).to match(/<span/) }
   end
 end
